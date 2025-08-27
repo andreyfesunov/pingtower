7eba564263262d612b7c7ac6db8853bd3ec21046
andreyfesunov <fsnv04@gmail.com>
2025-08-28 01:40:50 +0300

    chore(cleanup): remove default pages for plugin

--- plugin/src/components/App/App.tsx
+++ plugin/src/components/App/App.tsx
@@ -2,22 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from '../../constants';
 
-const PageOne = React.lazy(() => import('../../pages/PageOne'));
-const PageTwo = React.lazy(() => import('../../pages/PageTwo'));
-const PageThree = React.lazy(() => import('../../pages/PageThree'));
-const PageFour = React.lazy(() => import('../../pages/PageFour'));
+const EntrypointPage = React.lazy(() => import('../../pages/EntrypointPage'));
 
 function App() {
   return (
     <Routes>
-      <Route path={ROUTES.Two} element={<PageTwo />} />
-      <Route path={`${ROUTES.Three}/:id?`} element={<PageThree />} />
-
-      {/* Full-width page (this page will have no side navigation) */}
-      <Route path={ROUTES.Four} element={<PageFour />} />
-
-      {/* Default page */}
-      <Route path="*" element={<PageOne />} />
+      <Route path={ROUTES.Entrypoint} element={<EntrypointPage />} />
     </Routes>
   );
 }
--- plugin/src/module.tsx
+++ plugin/src/module.tsx
@@ -1,14 +1,14 @@
 import React, { Suspense, lazy } from 'react';
-import { AppPlugin, type AppRootProps } from '@grafana/data';
+import { AppPlugin } from '@grafana/data';
 import { LoadingPlaceholder } from '@grafana/ui';
 import type { AppConfigProps } from './components/AppConfig/AppConfig';
 
 const LazyApp = lazy(() => import('./components/App/App'));
 const LazyAppConfig = lazy(() => import('./components/AppConfig/AppConfig'));
 
-const App = (props: AppRootProps) => (
+const App = () => (
   <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyApp {...props} />
+    <LazyApp />
   </Suspense>
 );
 
